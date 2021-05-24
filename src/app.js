45c4ac1e06c9ce075b66e1e86b55422f1f488fde
Nigel Nahnfeldt <nigel.nahnfeldt@yahoo.com>
2021-05-24 10:41:26 -0400

    Fixed incompatible version errors

--- src/app.js
+++ src/app.js
@@ -13,15 +13,21 @@ const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water Bill'}));
 store.dispatch(addExpense({ description: 'Gas Bill'}));
+store.dispatch(setTextFilter('water'));
 
-store.dispatch(setTextFilter('bill'));
+setTimeout(() => {
+    store.dispatch(setTextFilter('rent'));
+}, 3000)
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpense(state.expenses, state.filters);
 console.log(visibleExpenses);
 
+
 const jsx = (
-    <AppRouter />
-);
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
 
 ReactDOM.render(jsx, document.getElementById("app"));
